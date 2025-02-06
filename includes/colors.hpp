5645fd32f4692b4ecdf7adfd76823add8a2b1e8a
Cyprien Ferreira <cyferrei@paul-f4Br7s3.clusters.42paris.fr>
2025-02-06 17:38:55 +0100

    [NEW]: for colors - Gonna be usefull

---
+++ includes/colors.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 16:19:38 by cyferrei          #+#    #+#             */
+/*   Updated: 2025/02/06 16:48:04 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+#define BOLD_ON "\033[1m"
+#define BOLD_OFF "\033[0m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define BLUE "\033[34m"
+#define ORANGE "\033[38;5;214m"
+
+#endif
