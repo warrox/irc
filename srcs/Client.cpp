0d7a06d7c871ebd79b5e8eef9449c84d84de68b2
cyferrei <ferreiracyprien@gmail.com>
2025-02-12 21:36:24 +0100

    [ADD]: add getters - setters for modes - also move Client constructor inside

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -6,13 +6,22 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:54:25 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/02/12 13:57:04 by whamdi           ###   ########.fr       */
+/*   Updated: 2025/02/12 21:30:23 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Client.hpp"
 
 #include <string>
+
+Client::Client() {
+	
+	this->_password = "";
+	this->mode_i = false;
+	this->mode_k = false;
+	this->mode_o = false;
+	this->mode_t = false;
+};
 
 void Client::setNick(std::string nick) {
 	this->_nick = nick;
@@ -28,6 +37,22 @@ void	Client::setHost(std::string host) {
 
 void	Client::setRealName(std::string realname) {
 	this->_real_name = realname;
+}
+
+void	Client::setModeI(bool enable) {
+	this->mode_i = enable;
+}
+
+void	Client::setModeT(bool enable) {
+	this->mode_t = enable;
+}
+
+void	Client::setModeK(bool enable) {
+	this->mode_k = enable;
+}
+
+void	Client::setModeO(bool enable) {
+	this->mode_o = enable;
 }
 
 std::string Client::getNick(void) const{
@@ -46,6 +71,21 @@ std::string Client::getRealName(void)const {
 	return this->_real_name;
 }
 
+bool Client::getModeI(void)const {
+	return (this->mode_i);
+}
+
+bool Client::getModeT(void)const {
+	return (this->mode_t);
+}
+
+bool Client::getModeK(void)const {
+	return (this->mode_k);
+}
+
+bool Client::getModeO(void)const {
+	return (this->mode_o);
+}
 
 std::string	Client::getChan(void)
 {
@@ -60,6 +100,4 @@ bool Client::getIsConnected(void)const {return this->_is_connected;}
 
 void Client::setPassword(std::string password){this->_password = password;}
 std::string Client::getPassword(void)const {return this->_password;}
-//Same as server, if you don't need a destructor, initialize it in the header file.
-//I doubt its usefullness here
-/*Client::~Client() {};*/
+
