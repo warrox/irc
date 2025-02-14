4fe12aa95f9f70912e9d50bd15f600354d83e364
Cyprien Ferreira <cyferrei@paul-f4Br6s7.clusters.42paris.fr>
2025-02-14 11:22:58 +0100

    [FIX]: fixed closing serveur when client disconnected itself

--- srcs/cmd_quit.cpp
+++ srcs/cmd_quit.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 18:17:06 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/02/13 18:23:32 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/02/14 10:15:24 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	Server::quit(int clientFd, std::string cmd) {
 	(void)cmd;
 	std::string quitMsg = ":" + _clients[clientFd].getNick() + "!~" + _clients[clientFd].getUser() + "@" + _clients[clientFd].getHost() + " QUIT :Client Quit\r\n";
 	
-	if (_clients.find(clientFd) != _clients.end()) {
-	sendAndLog(clientFd, ":" + this->_servername + " 421 " + _clients[clientFd].getNick() + " " + cmd + " :Unknown command\r\n");
-}
+	// 	if (_clients.find(clientFd) != _clients.end()) {
+	// 	sendAndLog(clientFd, ":" + this->_servername + " 421 " + _clients[clientFd].getNick() + " " + cmd + " :Unknown command\r\n");
+	// }
 	sendAndLog(clientFd, quitMsg);
 	close(clientFd);
 	_clients.erase(clientFd);
