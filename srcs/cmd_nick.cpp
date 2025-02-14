e08a6e2749dbe980dc1bf9beb646c7922cb8ca00
Cyprien Ferreira <cyferrei@paul-f4Br6s7.clusters.42paris.fr>
2025-02-14 13:46:36 +0100

    [FIX/ADD]: fixed nick

--- srcs/cmd_nick.cpp
+++ srcs/cmd_nick.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 10:41:57 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/02/14 11:20:16 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/02/14 12:36:12 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,16 +46,65 @@ void Server::nick(int clientFd, std::string cmd) {
 	std::cout << YELLOW << "|" << command << "|" << std::endl;
 	std::cout << YELLOW << "|" << nickname << "|" << std::endl;
 	if (nickname.empty()) {
-		sendAndLog(clientFd, ":localhost 461 " + _clients[clientFd].getNick() + " NICK :\r\n");
+		sendAndLog(clientFd, ":" + this->_servername + "431" + " " + _clients[clientFd].getNick() + " " + nickname + " :No nickname given\r\n");
 		return;
 	}
 	for (std::map<int, Client>::iterator match = _clients.begin(); match != _clients.end(); ++match) {
 		if (match->second.getNick() == nickname) {
-			setNewNick(clientFd, nickname);
-			sendAndLog(clientFd, this->get_prefix(clientFd) + " " + "NICK " + _clients[clientFd].getUser() + " " + _clients[clientFd].getNick()+ "\r\n");
+			// std::cout << BLUE << match->second.getNick() << std::endl;
+			sendAndLog(clientFd, ":" + this->_servername + "433" + " " + _clients[clientFd].getNick() + " " + nickname + " :Nickname in use\r\n");
+			if (_clients[clientFd].getNick().empty()) {
+				setNewNick(clientFd, "guest");
+				sendAndLog(clientFd, this->get_prefix(clientFd) + "NICK :" + _clients[clientFd].getUser() + " " + _clients[clientFd].getNick()+ "\r\n");
+			}
 			return;
 		}
 	}
 	_clients[clientFd].setNick(nickname);
-	sendAndLog(clientFd, this->get_prefix(clientFd) + " " + "NICK " + _clients[clientFd].getUser() + " " + _clients[clientFd].getNick() + "\r\n");
+	sendAndLog(clientFd, this->get_prefix(clientFd) + "NICK :" + _clients[clientFd].getUser() + " " + _clients[clientFd].getNick() + "\r\n");
 }
+
+// bool Server::isNickTaken(std::string nickname) {
+// 	for (std::map<int, Client>::const_iterator it = _clients.begin(); it != _clients.end(); ++it) {
+// 		if (it->second.getNick() == nickname)
+// 			return true;
+// 	}
+// 	return false;
+// }
+
+// void Server::setNewNick(int clientFd, std::string nickname) {
+// 	static int suf = 0;
+// 	std::stringstream new_nickname;
+
+// 	while (isNickTaken(nickname)) {
+// 		new_nickname.str("");
+// 		new_nickname << nickname << suf++;
+// 		nickname = new_nickname.str();
+// 	}
+
+// 	_clients[clientFd].setNick(nickname);
+// }
+
+// void Server::nick(int clientFd, std::string cmd) {
+// 	std::istringstream iss(cmd);
+// 	std::string command, nickname;
+// 	iss >> command >> nickname;
+
+// 	std::cout << YELLOW << "|" << command << "|" << std::endl;
+// 	std::cout << YELLOW << "|" << nickname << "|" << std::endl;
+
+// 	if (nickname.empty()) {
+// 		sendAndLog(clientFd, ":localhost 431 * :No nickname given\r\n");
+// 		return;
+// 	}
+
+// 	if (isNickTaken(nickname)) {
+// 		sendAndLog(clientFd, ":localhost 433 " + _clients[clientFd].getNick() + " " + nickname + " :Nickname is already in use\r\n");
+// 		setNewNick(clientFd, nickname);
+// 	} else {
+// 		_clients[clientFd].setNick(nickname);
+// 	}
+
+// 	sendAndLog(clientFd, this->get_prefix(clientFd) + " NICK " + _clients[clientFd].getNick() + "\r\n");
+// }
+
