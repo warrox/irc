88e17c8c6d8bd3d1f111c8f52b3140cf764515f7
Warren Hamdi <warren.hamdi@gmail.com>
2025-02-24 14:59:23 +0100

    [IRC] --> [TOPIC] fix bug with mode T

--- srcs/cmd_topic.cpp
+++ srcs/cmd_topic.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 10:44:28 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/02/18 14:09:08 by whamdi           ###   ########.fr       */
+/*   Updated: 2025/02/24 14:57:37 by whamdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,7 @@ void Server::topic(int clientFd, std::string cmd)
         topic_name.erase(0, 2);
 
     std::string actual_chan = this->_clients[clientFd].getChan();
-    std::cout << "cmd brut : " << cmd << std::endl;
-
-    if (actual_chan != "NO" && this->_clients[clientFd].getModeO())
+    if (actual_chan != "NO" &&  !this->_channels[chan].getModeT())
     {
         this->_channels[chan].setTopic(topic_name);
 
@@ -43,7 +41,18 @@ void Server::topic(int clientFd, std::string cmd)
 		// this->_channels[chan].topicBroadcast(ClientFd, *this, response);
 		// [server] --> :whamdi!whamdi@localhost TOPIC #pd :haah
         // send(clientFd, response.c_str(), response.size(), 0);
-    }
+    }else if(actual_chan != "NO" && this->_channels[chan].getModeT())
+	{
+		if(this->_clients[clientFd].getModeO())
+		{
+			this->_channels[chan].setTopic(topic_name);
+
+	        std::string response = ":" + this->_servername + " TOPIC " + chan + " :" + topic_name + "\r\n";
+
+			this->sendAndLog(clientFd, response);	
+			this->_channels[chan].broadcast(clientFd, *this, response, true);
+		}
+	}
     else
     {
         std::string errorMsg = ":" + this->_servername + " 442 " + this->_clients[clientFd].getNick() + " " + chan + " :You're not on that channel\r\n";
